9b4187b73c5dcb5e6ed1387e5fb63cbd126022f9
Sean Hamm <seanhamm28@gmail.com>
2024-04-14 13:28:30 -0700

    assignment 2

---
+++ test.js
@@ -0,0 +1,43 @@
+
+/*
+
+Operator 	  Description
+    + 	        Addition
+    -       	Subtraction
+    *        	Multiplication
+    **       	Exponentiation (ES2016)
+    /       	Division
+    %       	Modulus (Remainder)
+    ++ 	        Increment
+    --      	Decrement
+    
+    */
+
+
+/* 3 Examples of basic arithmetic in JS */
+
+console.log(1+2)
+console.log(2-1)
+console.log(3%2)
+
+
+/* 2 Examples of declaring variables in JS */
+
+let a = 10
+console.log(a)
+
+let b = 2
+console.log(b)
+
+/* 2 Examples of using variables in arithmetic operations in JS */
+
+console.log(a +2)
+
+console.log(b *a)
+
+const MAX = 57
+let actual = MAX - 13
+let percentage = actual / MAX
+
+
+
